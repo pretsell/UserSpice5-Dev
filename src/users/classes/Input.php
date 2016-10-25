c667a235127127f658ac10207744bb1202fe55c7
Peter L. Bowers <plbowers@gmail.com>
2016-10-25 14:00:04 +0200

    Simplify Input::get() and allow for get/post/request differentiation

--- src/users/classes/Input.php
+++ src/users/classes/Input.php
@@ -33,35 +33,30 @@ class Input {
 		}
 	}
 
-	public static function get($item){
-		if (isset($_POST[$item])) {
-			/*
-			If the $_POST item is an array, process each item independently, and return array of sanitized items.
-			*/
-			if (is_array($_POST[$item])){
-				$postItems=array();
-				foreach ($_POST[$item] as $postItem){
-					$postItems[]=self::sanitize($postItem);
-				}
-				return $postItems;
-			}else{
-				return self::sanitize($_POST[$item]);
-			}
-			
-		} elseif(isset($_GET[$item])){
-			/*
-			If the $_GET item is an array, process each item independently, and return array of sanitized items.
-			*/			
-			if (is_array($_GET[$item])){
-				$getItems=array();
-				foreach ($_GET[$item] as $getItem){
-					$getItems[]=self::sanitize($getItem);
-				}
-				return $getItems;
-			}else{
-				return self::sanitize($_GET[$item]);
-			}
+	public static function get($item, $method='request'){
+		switch (strtolower($method)) {
+			case 'get':
+				$src = &$_GET;
+				break;
+			case 'post':
+				$src = &$_POST;
+				break;
+			default:
+				$src = &$_REQUEST;
+				break;
 		}
+		if (isset($src[$item])) {
+			# If the item is an array, process each item independently, and return array of sanitized items.
+			if (is_array($src[$item])){
+				$items=array();
+				foreach ($src[$item] as $k => $item){
+					$items[$k]=self::sanitize($item);
+				}
+				return $items;
+			}else{
+				return self::sanitize($src[$item]);
+			}
+		} 
 		return '';
 	}
 
