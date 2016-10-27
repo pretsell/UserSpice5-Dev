6981d52ec42dde8c2cbf6a7e831670926d6bd19c
Peter L. Bowers <plbowers@gmail.com>
2016-10-27 07:21:42 +0200

    getting rid of debug statement

--- src/users/classes/DB.php
+++ src/users/classes/DB.php
@@ -46,7 +46,6 @@ class DB {
 	public function query($sql, $params = array()) {
 		$this->_queryCount++;
 		$this->_error = false;
-		#echo "DEBUG: sql=$sql<br />\n";
 		if ($this->_query = $this->_pdo->prepare($sql)) {
 			$x = 1;
 			if (count($params)) {
