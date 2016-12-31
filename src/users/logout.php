719f27969c99bf947fb6098f070696f5ab815444
Peter L. Bowers <plbowers@gmail.com>
2016-12-31 15:26:59 +0100

    renamed constants

--- src/users/logout.php
+++ src/users/logout.php
@@ -2,14 +2,14 @@
 
 require_once 'init.php';
 
-if(file_exists(ABS_US_ROOT.US_URL_ROOT.'usersc/scripts/just_before_logout.php')){
-	require_once ABS_US_ROOT.US_URL_ROOT.'usersc/scripts/just_before_logout.php';
+if(file_exists(US_DOC_ROOT.US_URL_ROOT.'usersc/scripts/just_before_logout.php')){
+	require_once US_DOC_ROOT.US_URL_ROOT.'usersc/scripts/just_before_logout.php';
 }else{
 	//Feel free to change where the user goes after logout!
 }
 $user->logout();
-if(file_exists(ABS_US_ROOT.US_URL_ROOT.'usersc/scripts/just_after_logout.php')){
-	require_once ABS_US_ROOT.US_URL_ROOT.'usersc/scripts/just_after_logout.php';
+if(file_exists(US_DOC_ROOT.US_URL_ROOT.'usersc/scripts/just_after_logout.php')){
+	require_once US_DOC_ROOT.US_URL_ROOT.'usersc/scripts/just_after_logout.php';
 }else{
 	//Feel free to change where the user goes after logout!
 	Redirect::to(US_URL_ROOT.configGet('redirect_logout'));
