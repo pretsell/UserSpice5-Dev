8efe66e00983bb52f8d8ae7cbc0f40236b7986f3
Peter L. Bowers <plbowers@gmail.com>
2016-12-31 15:26:09 +0100

    added Form_Form, Form_Col, Form_Row, Form_Well as straight inheritors

--- src/users/local/Classes/FormTab.php
+++ src/users/local/Classes/FormTab.php
@@ -3,3 +3,11 @@ class FormTab_Contents extends US_FormTab_Contents {
 }
 class FormTab_Pane extends US_FormTab_Pane {
 }
+class Form_Form extends US_Form_Form {
+}
+class Form_Col extends US_Form_Col {
+}
+class Form_Row extends US_Form_Row {
+}
+class Form_Well extends US_Form_Well {
+}
