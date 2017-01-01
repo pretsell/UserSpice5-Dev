1e14036797d136e6a9700266f31e9be752b89d37
Peter L. Bowers <plbowers@gmail.com>
2017-01-01 21:19:26 +0100

    removed create wells (use admin_group.php instead), get rid of old form source at the end of the script

--- users/us_core/forms/admin_groups.php
+++ users/us_core/forms/admin_groups.php
@@ -41,25 +41,6 @@ if (isset($mode) && strtoupper($mode) == 'ROLE') {
 # We need 2 separate <form>...</form> because of "required" fields
 # in the "create" section which are not required if we are deleting
 $myForm = new Form ([
-    'createWell' => new Form_Well ([
-        'createForm' => new Form_Form ([
-            '<h3>'.lang('CREATE_'.$mode).'</h3>',
-            'name' => new FormField_Text ([
-                'dbfield' => 'groups.name',
-                #'debug' => 3,
-                'new_valid' => ['action'=>'add'],
-            ]),
-            'short_name' => new FormField_Text ([
-                'dbfield' => 'groups.short_name',
-                #'debug' => 3,
-                'new_valid' => ['action'=>'add'],
-            ]),
-            'create' => new FormField_ButtonSubmit ([
-                'name' => 'create',
-                'display' => lang('CREATE_'.$mode),
-            ]),
-        ], ['Form_Name' => 'createForm']),
-    ], ['Well_Class' => 'well-sm']),
     'deleteGroupForm' => new Form_Form ([
         '<h4>'.lang($mode.'_LIST_TITLE').'</h4>',
         'groupList' => new FormField_Table ([
@@ -70,20 +51,28 @@ $myForm = new Form ([
                 '<th>'.lang($mode.'_SHORT_NAME').'</th>'.
                 '<th>'.lang('GROUPTYPE_NAME_LABEL').'</th>'.
                 ($mode == 'ROLE' ? '' : '<th>'.lang('ROLE_LIST_TITLE').'</th>'),
-            'table_data_cells' => '<td>{CHECKBOX}</td>'.
+            'table_data_cells' => '<td>{DELETE_CHECKBOX}</td>'.
                 '<td><a href="'.$childForm.'?id={ID}">{NAME}</a></td>'.
                 '<td>{SHORT_NAME}</td>'.
                 '<td><a href="admin_grouptypes.php?id={grouptype_id}">{GROUPTYPE_NAME}</a></td>'.
                 ($mode == 'ROLE' ? '' : '<td>{ROLES}</td>'  ),
             'nodata' => '<p>'.lang('NO_ROLES_ASSIGNED').'</p>',
             'Table_Class' => 'table table-bordered table-condensed',
-        ], ['Form_Name' => 'deleteGroupForm']),
+            'Delete_Label' => lang('MARK_TO_DELETE'),
+        ], [
+            'debug' => 5,
+            'Form_Name' => 'deleteGroupForm',
+        ]),
         'save' => new FormField_ButtonSubmit ([
-            'display' => 'Delete Selected',
+            'display' => lang('DELETE_SELECTED_'.$mode.'S'),
+        ]),
+        'create' => new FormField_ButtonAnchor ([
+            'display' => lang('CREATE_'.$mode),
+            'link' => $childForm,
         ])
     ], ['Form_Name' => 'deleteForm']),
 ], [
-    'title' => lang('ADMINISTRATE_'.$mode),
+    'title' => lang('ADMIN_'.$mode.'S_TITLE'), // ADMIN_GROUPS_TITLE or ADMIN_ROLES_TITLE
     'exclude_elements' => ['openForm', 'closeForm'],
     'dbtable' => 'groups',
 ]);
@@ -94,15 +83,6 @@ if(Input::exists('post')) {
     if($deletions = Input::get('deleteGroups', 'post')) {
         if ($deletion_count = deleteGroups($deletions, $errors)) {
             $successes[] = lang("GROUP_DELETIONS_SUCCESSFUL", array($deletion_count));
-        }
-    }
-
-    //Create new group
-    if(!empty($_POST['name']) || !empty($_POST['create'])) {
-        #var_dump($_POST);
-        $myForm->setNewValues($_POST);
-        if ($myForm->insertIfValid($errors)) {
-            $successes[] = lang('GROUP_ADD_SUCCESSFUL', $myForm->getField('name')->getNewValue());
         }
     }
 }
@@ -129,92 +109,3 @@ foreach ($groups as $gd) {
 $myForm->getField('groupList')->setRepData($groupData);
 
 echo $myForm->getHTML(['errors'=>$errors, 'successes'=>$successes]);
-?>
-
-<div class="container">
-    <div class="row">
-	<div class="col-xs-12">
-	<h1 class="text-center">UserSpice Dashboard <?=configGet('version')?></h1>
-	<!-- ?php require_once US_DOC_ROOT.US_URL_ROOT.'users/includes/admin_nav.php'; ? -->
-	</div>
-      <div class="col-xs-12">
-		<?php
-		resultBlock($errors, $successes);
-		?>
-
-		<form name='adminGroups' action='admin_groups.php' method='post'>
-            <input type="hidden" name="mode" value="<?= $mode ?>" />
-        <div>
-        <h2><?= lang('ADMINISTRATE_'.$mode) ?></h2>
-        <div class="well">
-		  <h4>Create a new <?= lang('CREATE_'.$mode) ?></h4>
-			<label><?= lang($mode.'_NAME') ?>: </label>
-			<input type='text' name='name' />
-			<span class="glyphicon glyphicon-info-sign" title="<?= $validation->describe('name') ?>"></span>&nbsp;&nbsp;
-			<label><?= lang($mode.'_SHORT_NAME') ?>: </label>
-			<input type='text' name='short_name' />
-			<span class="glyphicon glyphicon-info-sign" title="<?= $validation->describe('short_name') ?>"></span>&nbsp;&nbsp;
-			<input class='btn btn-primary' type='submit' name='create' value='Create' /><br><br>
-        </div>
-		<br>
-		<table class='table table-hover table-list-search'>
-        <tr>
-            <th colspan="3"><h3><?= lang($mode.'_LIST_TITLE') ?></h3></th>
-        </tr>
-		<tr>
-	  	    <th><?= lang('DELETE') ?></th><th><?= lang($mode.'_NAME') ?></th><th><?= lang($mode.'_SHORT_NAME') ?></th><th><?= lang('GROUP_TYPE') ?></th>
-            <?php
-            if ($hasRoles && $mode != 'ROLE') {
-            ?>
-                <th>Roles</th>
-            <?php
-            }
-            ?>
-		</tr>
-		<?php
-		//List each group where is_role==n  n=0:(normal groups)/n=1:(role group)
-        $roleVal = (($mode == 'ROLE') ? 1 : 0);
-		foreach ($groupData as $g) {
-            if ($g->is_role == $roleVal) { ?>
-				<tr>
-					<td><input type='checkbox' name='delete[<?=$g->id?>]' value='<?=$g->id?>' ></td>
-					<td><a href='<?= $mode == 'ROLE' ? 'admin_role.php' : 'admin_group.php' ?>?id=<?=$g->id?>'><?=$g->name?></a></td>
-					<td><?=$g->short_name?></td>
-					<td><a href='admin_grouptypes.php?id=<?=$g->grouptype_id?>'><?=findValInArrayOfObject($groupTypes, 'id', $g->grouptype_id, 'name')?></a></td>
-                    <td>
-                    <?php
-                    if ($hasRoles && $mode != 'ROLE') {
-                        $sep = '';
-                        foreach (fetchRolesByGroup($g->id) as $role) {
-                            echo $sep.'<span style="font-weight: bold">'.$role->short_name.'</span>: '.$role->fname.' '.$role->lname.' ('.$role->username.')';
-                            $sep = ', ';
-                        }
-                    } ?>
-                    </td>
-				</tr>
-				<?php
-				$count++;
-            }
-		}
-		?>
-		</table>
-		<input type="hidden" name="csrf" value="<?=Token::generate();?>" >
-		<input class='btn btn-primary' type='submit' name='Submit' value='Save Changes' /><br><br>
-		</form>
-
-          <!-- End of main content section -->
-
-      </div>
-    </div>
-</div>
-
-
-    <!-- /.row -->
-
-    <!-- footers -->
-<?php require_once US_DOC_ROOT.US_URL_ROOT.'users/includes/page_footer.php'; // the final html footer copyright row + the external js calls ?>
-
-<!-- Place any per-page javascript here -->
-<script src="js/search.js" charset="utf-8"></script>
-
-<?php require_once US_DOC_ROOT.US_URL_ROOT.'users/includes/html_footer.php'; // currently just the closing /body and /html ?>
