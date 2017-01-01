6e8c356127634b2a22df745495c30fa17cd8e524
Peter L. Bowers <plbowers@gmail.com>
2017-01-01 20:26:39 +0100

    new script, brings all scripts for editing settings table under one roof

---
+++ users/admin_settings.php
@@ -0,0 +1,5 @@
+<?php
+$formName = 'admin_settings.php';
+#$enableMasterHeaders = $enableMasterFooters = true;
+require_once 'c:/wamp/www/UserSpice5-Dev/users/z_us_root.php';
+require_once US_ROOT_DIR.'us_core/master_form.php';
