60a411a43c8fec862a4757a08659ba298db27fb9
Peter L. Bowers <plbowers@gmail.com>
2017-03-01 21:34:41 +0100

    logout.php now works with StateResponse

--- users/logout.php
+++ users/logout.php
@@ -2,16 +2,8 @@
 
 require_once 'init.php';
 
-if(file_exists(US_DOC_ROOT.US_URL_ROOT.'usersc/scripts/just_before_logout.php')){
-	require_once US_DOC_ROOT.US_URL_ROOT.'usersc/scripts/just_before_logout.php';
-}else{
-	//Feel free to change where the user goes after logout!
-}
+$prelogout = new StateResponse_PreLogout;
+$prelogout->respond();
 $user->logout();
-if(file_exists(US_DOC_ROOT.US_URL_ROOT.'usersc/scripts/just_after_logout.php')){
-	require_once US_DOC_ROOT.US_URL_ROOT.'usersc/scripts/just_after_logout.php';
-}else{
-	//Feel free to change where the user goes after logout!
-	Redirect::to(US_URL_ROOT.configGet('redirect_logout'));
-}
-?>
+$postlogout = new StateResponse_Logout;
+$postlogout->respond();
