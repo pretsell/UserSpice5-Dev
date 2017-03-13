46b732bcb6edc61f072aebc0d95f3fd93bdeb578
Peter L. Bowers <plbowers@gmail.com>
2017-03-13 18:04:11 +0100

    added example in tutorial/FormField_Table1.php

---
+++ users/tutorial/FormField_Table1.php
@@ -0,0 +1,5 @@
+<?php
+$formName = 'FormField_Table1.php';
+#$enableMasterHeaders = $enableMasterFooters = true;
+require_once 'C:/wamp/www/UserSpice5-Dev/users/z_us_root.php';
+require_once US_ROOT_DIR.'core/master_form.php';
---
+++ users/tutorial/forms/FormField_Table1.php
@@ -0,0 +1,22 @@
+<?php
+$db = DB::getInstance();
+$fooData = $db->queryAll('foo')->results(); // contains columns `id` and `foo`
+$myForm = new Form([
+    'delete' => new FormField_Table([
+        'data' => $fooData,
+        'th_row' => '<th>Mark for Deletion</th><th>Foo</th>',
+        'td_row' => '<td>{CHECKBOX_ID}</td><td>{FOO}</td>',
+        'checkbox_label' => 'Mark for Deletion',
+    ]),
+    'delete_button' => new FormField_ButtonDelete([
+        'display' => 'Delete Marked Rows',
+    ]),
+], [
+    'autoshow' => 1,
+]);
+
+if (Input::exists()) {
+    echo "This is what the \$_POST looks like:";
+    var_dump($_POST);
+    echo "<br />\nIf `'default'=>'processing'` had been set for the form then any rows matching the IDs listed in the `delete` array (above) would have been automatically deleted.";
+}
