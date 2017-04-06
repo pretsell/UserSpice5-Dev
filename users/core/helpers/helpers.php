fdfb8aec1a079733fcfdf4b462124a3fa02a0ce1
Peter L. Bowers <plbowers@gmail.com>
2017-04-06 21:09:55 +0200

    added Australia, small refactor for calling from other contexts

--- users/core/helpers/helpers.php
+++ users/core/helpers/helpers.php
@@ -222,13 +222,14 @@ function output_message($message) {
 return $message;
 }
 
-function timezone_dropdown($selected){
+function timezone_list() {
 	$regions = array(
 		'Africa' => DateTimeZone::AFRICA,
 		'America' => DateTimeZone::AMERICA,
 		'Antarctica' => DateTimeZone::ANTARCTICA,
 		'Asia' => DateTimeZone::ASIA,
 		'Atlantic' => DateTimeZone::ATLANTIC,
+		'Australia' => DateTimeZone::AUSTRALIA,
 		'Europe' => DateTimeZone::EUROPE,
 		'Indian' => DateTimeZone::INDIAN,
 		'Pacific' => DateTimeZone::PACIFIC
@@ -240,13 +241,17 @@ function timezone_dropdown($selected){
 		foreach($zones as $timezone){
 			// Lets sample the time there right now
 			$time = new DateTime(NULL, new DateTimeZone($timezone));
-			// Us dumb Americans can't handle millitary time
+			// Us dumb Americans can't handle military time
 			$ampm = $time->format('H') > 12 ? ' ('. $time->format('g:i a'). ')' : '';
 			// Remove region name and add a sample time
 			$timezones[$name][$timezone] = substr($timezone, strlen($name) + 1) . ' - ' . $time->format('H:i') . $ampm;
 		}
 	}
+    return $timezones;
+}
 
+function timezone_dropdown($selected) {
+    $timezones = timezone_list();
 	// View
 	$dropdownString='<select class="form-control" name="timezone_string" id="timezone_string">';
 	foreach($timezones as $region => $list)
