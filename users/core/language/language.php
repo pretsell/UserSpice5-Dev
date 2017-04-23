d2b9f11409652a42ca963332c5627faf168eb1f3
Peter L. Bowers <plbowers@gmail.com>
2017-04-23 19:46:55 +0200

    moved from storing "english.php" to "en" in settings table allowed storing of extra language tokens in lang table in database - loaded here

--- users/core/language/language.php
+++ users/core/language/language.php
@@ -23,7 +23,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 //actual logic will eventually go here
-$curLang = configGet('site_language', 'english.php');
+$curLangCode = configGet('site_language', 'en');
+$langFiles = configGet('language_files');
+if (isset($langFiles[$curLangCode])) {
+    $curLang = $langFiles[$curLangCode];
+} else {
+    throw new Exception("ERROR: Language code `$curLangCode` is not configured in `language_files` in local/config.php");
+}
 if ($curLang != 'english.php') {
     # these values will be over-ridden by the language settings below, but since
     # english is likely to be the most complete language file we will start with
@@ -39,4 +45,9 @@ if (file_exists($locallang = US_ROOT_DIR."local/language/$curLang")) {
 }
 if (file_exists($devlang = configGet('alt_dev_path')."/language/$curLang")) {
     require_once $devlang;
+}
+$db = DB::getInstance();
+$rows = $db->query("SELECT token, message FROM $T[lang] WHERE lang = ?", [$curLangCode])->results();
+foreach ($rows as $row) {
+    $lang[$row->token] = $row->message;
 }
