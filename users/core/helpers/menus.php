75685f136f01672e251634c85ea25df0cff81410
Peter L. Bowers <plbowers@gmail.com>
2017-04-23 19:42:45 +0200

    re-worked menus from scratch, allowed multi-level menus

--- users/core/helpers/menus.php
+++ users/core/helpers/menus.php
@@ -1,53 +1,196 @@
 <?php
-function _assert($expr, $msg) {
-    if (!$expr) {
-        print "<br/><b>ASSERTION FAIL: </b>{$msg}<br>";
+function getMenu($menuTitle, $logo=null, $stub=false) {
+    global $footerSnippets;
+    $menuSQL = menuSQL($menuTitle, $bindVals);
+    $menuItems = prepMenu($menuSQL, $bindVals);
+    # This enables SmartMenus on this menu
+    /* TURNS OUT THIS IS POINTLESS WHEN COMBINED WITH SMARTMENUS BOOTSTRAP ADDON
+    $footerSnippets[] = '
+        <script>
+            $(function() {
+                $("#navbar_'.$menuTitle.'").smartmenus({
+                    noMouseOver:true,
+                    showOnClick: true,
+                    subIndicators: false
+                });
+            });
+        </script>';
+    */
+    return getMenuHTML($menuTitle, $menuItems, $logo, $stub);
+}
+function menuSQL($menuTitle, &$bindVals, $forceAll=false) {
+    global $T, $user, $lang;
+    if ($user->id()) {
+        $userId = $user->id();
+    } else {
+        $userId = -1;
     }
+    if ($forceAll) {
+        $where = '';
+    } else {
+        if (isset($user) && $user->isLoggedIn()) {
+            $where = ' AND (logged_in >= 0)';
+        } else {
+            $where = ' AND (logged_in <= 0)';
+        }
+        if (isset($user) && $user->isAdmin()) {
+            $where .= " AND (admin >= 0)";
+        } else {
+            $where .= " AND (admin <= 0)";
+        }
+        if (isset($user) && $user->data() && $user->data()->email_verified) {
+            $where .= " AND (email_verified >= 0)";
+        } else {
+            $where .= " AND (email_verified <= 0)";
+        }
+        if (isset($user) && $user->data() && $user->data()->active) {
+            $where .= " AND (active >= 0)";
+        } else {
+            $where .= " AND (active <= 0)";
+        }
+    }
+    $sql = "SELECT menus.id, menu_title, label_token, parent, logged_in,
+                display_order, icon_class,
+                CONCAT(IF(link <> '', link, pages.page), link_args) AS link
+              FROM $T[menus] menus
+              JOIN $T[pages] pages ON (pages.id = menus.page_id)
+             WHERE menu_title = ? ";
+    $bindVals = [$menuTitle];
+    if (!$forceAll && !$user->isAdmin()) {
+        $sql .= "AND (pages.private = 0 OR
+                   (pages.private = 1 AND EXISTS
+                        (SELECT *
+                          FROM $T[groups_pages] gp
+                          JOIN $T[groups_users] gu
+                                ON (gp.group_id = gu.group_id)
+                        WHERE gp.page_id = menus.page_id
+                          AND gu.user_id = ?))) ";
+        $bindVals[] = $userId;
+        $sql .= "AND (menus.private = 0 OR
+                   (menus.private = 1 AND EXISTS
+                        (SELECT *
+                          FROM $T[groups_menus] gm
+                          JOIN $T[groups_users] gu
+                                ON (gm.group_id = gu.group_id)
+                        WHERE gm.menu_id = menus.id
+                          AND gu.user_id = ?))) ";
+        $bindVals[] = $userId;
+    }
+    $sql .= " $where
+            UNION
+            SELECT menus.id, menu_title, label_token, parent, logged_in,
+                display_order, icon_class,
+                CONCAT(link, link_args) AS link
+              FROM $T[menus] menus
+             WHERE menu_title = ?
+               AND (page_id IS NULL OR page_id = 0)
+               $where
+            ORDER BY parent, display_order";
+    $bindVals[] = $menuTitle;
+    return $sql;
 }
-
-function prepareMenuTree($menuResults) {
-	/*
-	Get instance of tree manager and build the tree
-	*/
-	$treeManager = treeManager::get();
-	$menuTree = $treeManager->getTree($menuResults, 'id','parent','display_order');
-    #var_dump($menuTree);
-	/*
-	Indent the tree
-	*/
-	//$menuTree = $treeManager->slapTree($recordsTree, 1 ); //1 for indent count
-
-	return $menuTree;
+function prepMenu($menuSQL, $bindVals) {
+    global $user, $lang;
+    #pre_r( "SQL=$sql<br />\n");
+    #dbg("menuTitle=$menuTitle, userId=$userId");
+    $db = DB::getInstance();
+    if ($db->query($menuSQL, $bindVals)->error()) {
+        throw new Exception("ERROR: ".$db->errorString());
+        die;
+    }
+    foreach ($db->results(true) as $row) {
+        #dbg("LABEL: ".$row['label_token']);
+        $row['children'] = []; // default
+        $subs[$row['parent']][] = $row;
+    }
+    if (!isset($subs)) {
+        return []; // default to an empty menu
+    }
+    #dbg("SUBS");
+    #var_dump($subs);
+    $menu = [];
+    foreach ((array)@$subs[-1] as $sub) { // menus without a parent
+        $menu[$sub['id']] = $sub;
+        $lookup[$sub['id']] = &$menu[$sub['id']];
+    }
+    unset($subs[-1]);
+    $toDel = [];
+    $lastCount = -1;
+    while ($subs && $lastCount != sizeof($subs)) {
+        foreach ($subs as $parent => $s) {
+            #dbg("parent=$parent, s=");
+            #var_dump($s);
+            if (isset($lookup[$parent])) {
+                $lookup[$parent]['children'] = $s;
+                foreach ($lookup[$parent]['children'] as &$c) {
+                    $lookup[$c['id']] = &$c;
+                }
+                $toDel[] = $parent;
+            }
+        }
+        if (!$toDel) {
+            break; // didn't assign any - just get out and ignore the rest
+        }
+        #var_dump($toDel);
+        $lastCount = sizeof($subs);
+        foreach ($toDel as $d) {
+            unset($subs[$d]);
+        }
+        $toDel = [];
+    }
+    #dbg("Returning menu $menuTitle");
+    #var_dump($menu);
+    return $menu;
 }
+function getMenuHTML($menuTitle, $menuItems, $logo=null, $stub=false) {
+    global $lang, $user;
 
-function prepareIndentedMenuTree($menuResults) {
-	/*
-	Get instance of tree manager and build the tree
-	*/
-	$treeManager = treeManager::get();
-	$menuTree = $treeManager->getTree($menuResults, 'id','parent','display_order');
-	/*
-	Indent the tree
-	*/
-	$menuIndentedTree = $treeManager->slapTree($menuTree, 1,'menu_title' ); //1 for indent count
+    # Setting this is faster than going through another replace
+    $lang['MENU_MAIN_USERNAME_MACRO'] = $user->username();
 
-	return $menuIndentedTree;
+    $html = '
+        <nav class="navbar navbar-default">
+        <div class="container-fluid">
+          <div class="navbar-header">
+        	<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar_'.$menuTitle.'" aria-expanded="false" aria-controls="navbar">
+        		<span class="sr-only">Toggle navigation</span>
+        		<span class="icon-bar"></span>
+        		<span class="icon-bar"></span>
+        		<span class="icon-bar"></span>
+        	</button>';
+    if ($logo) {
+        $html .= '<a href="'.US_URL_ROOT.'"><img src="'.$logo.'"></img></a>';
+    }
+    $html .= '
+          </div>
+          <div id="navbardiv_'.$menuTitle.'" class="navbar-collapse collapse">
+        	<ul id="navbar_'.$menuTitle.'" class="sm nav navbar-nav navbar-right">';
+    foreach ($menuItems as $key => $value) {
+        $html .= getMenuItemHTML($value, 0, $stub);
+    }
+    $html .= '
+        	</ul>
+          </div><!--/.nav-collapse -->
+        </div><!--/.container-fluid -->
+        </nav>';
+    return $html;
 }
-
-function prepareDropdownString($menuItem) {
-	$itemString='';
-	$itemString.='<li class="dropdown">';
-	$itemString.='<a href="" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="'.$menuItem['icon_class'].'"></span> '.$menuItem['label'].' <span class="caret"></span></a>';
-	$itemString.='<ul class="dropdown-menu">';
-	foreach ($menuItem['children'] as $childItem) {
-		$itemString.=prepareItemString($childItem);
-	}
-	$itemString.='</ul></li>';
-	return $itemString;
-}
-
-function prepareItemString($menuItem) {
-	$itemString='';
-	$itemString.='<li><a href="'.$menuItem['link'].'"><span class="'.$menuItem['icon_class'].'"></span> '.$menuItem['label'].'</a></li>';
-	return $itemString;
+function getMenuItemHTML($menuItem, $level, $stub=false) {
+    if ($menuItem['children']) {
+    	$itemString ='<li class="dropdown">';
+    	$itemString.='<a href="'.$menuItem['link'].'" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="'.$menuItem['icon_class'].'"></span> '.lang($menuItem['label_token']).' <span class="caret"></span></a>';
+    	$itemString.='<ul class="dropdown-menu">';
+    	foreach ($menuItem['children'] as $childItem) {
+    		$itemString .= getMenuItemHTML($childItem, $level+1, $stub);
+    	}
+    	$itemString.='</ul></li>';
+    	return $itemString;
+    } else {
+        if ($stub) {
+        	$itemString='<li><a onclick="alert(\'Would have gone to `'.$menuItem['link'].'`\'); return false;" href="'.$menuItem['link'].'"><span class="'.$menuItem['icon_class'].'"></span> '.lang($menuItem['label_token']).'</a></li>';
+        } else {
+        	$itemString='<li><a href="'.$menuItem['link'].'"><span class="'.$menuItem['icon_class'].'"></span> '.lang($menuItem['label_token']).'</a></li>';
+        }
+    	return $itemString;
+    }
 }
