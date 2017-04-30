b187419754a292e7f03ad530ac4c37f510a53c86
Peter L. Bowers <plbowers@gmail.com>
2017-04-30 15:26:15 +0200

    whitespace for readability

--- users/local/Classes/FormFieldTypes.php
+++ users/local/Classes/FormFieldTypes.php
@@ -1,14 +1,14 @@
 <?php
 /* Feel free to customize these classes */
-class FormField_ButtonAnchor extends US_FormField_ButtonAnchor {
+class FormField_ButtonAnchor  extends US_FormField_ButtonAnchor {
 }
-class FormField_ButtonSubmit extends US_FormField_ButtonSubmit {
+class FormField_ButtonSubmit  extends US_FormField_ButtonSubmit {
 }
-class FormField_ButtonDelete extends US_FormField_ButtonDelete {
+class FormField_ButtonDelete  extends US_FormField_ButtonDelete {
 }
-class FormField_Checkbox extends US_FormField_Checkbox {
+class FormField_Checkbox      extends US_FormField_Checkbox {
 }
-class FormField_Checklist extends US_FormField_Checklist {
+class FormField_Checklist     extends US_FormField_Checklist {
 }
 # "MultiCheckbox" is an alias for "Checklist" (note it extends FormField_Checklist
 # and not US_FormField_Checklist)
@@ -17,27 +17,29 @@ class FormField_MultiCheckbox extends FormField_Checklist {
     # unless you are deliberately separating the functionality of what was
     # originally designed to be a simple alias
 }
-class FormField_File extends US_FormField_File {
+class FormField_File          extends US_FormField_File {
 }
-class FormField_Hidden extends US_FormField_Hidden {
+class FormField_Hidden        extends US_FormField_Hidden {
 }
-class FormField_MultiHidden extends US_FormField_MultiHidden {
+class FormField_HTML          extends US_FormField_HTML {
 }
-class FormField_Password extends US_FormField_Password {
+class FormField_MultiHidden   extends US_FormField_MultiHidden {
 }
-class FormField_Radio extends US_FormField_Radio {
+class FormField_Password      extends US_FormField_Password {
 }
-class FormField_Recaptcha extends US_FormField_Recaptcha {
+class FormField_Radio         extends US_FormField_Radio {
 }
-class FormField_SearchQ extends US_FormField_SearchQ {
+class FormField_ReCaptcha     extends US_FormField_ReCaptcha {
 }
-class FormField_Select extends US_FormField_Select {
+class FormField_SearchQ       extends US_FormField_SearchQ {
 }
-class FormField_Table extends US_FormField_Table {
+class FormField_Select        extends US_FormField_Select {
 }
-class FormField_TabToC extends US_FormField_TabToC {
+class FormField_Table         extends US_FormField_Table {
 }
-class FormField_Text extends US_FormField_Text {
+class FormField_TabToC        extends US_FormField_TabToC {
 }
-class FormField_Textarea extends US_FormField_Textarea {
+class FormField_Text          extends US_FormField_Text {
+}
+class FormField_Textarea      extends US_FormField_Textarea {
 }
